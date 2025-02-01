9313a167aa042eb8d0dddf2c5fbe906140c561ad
mohammed22588 <113137681+mohammed22588@users.noreply.github.com>
2025-02-01 22:29:28 +0300

    Delete Telegram.py

--- Telegram.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def send_message_volume(message):
-    bot_token = "8173508973:AAFBIrMcdpweiaE1a9L4IVqolynCmz_lMw8"
-    bot_chatID = "-2493334574"
-    send_text = "https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+bot_chatID+"&parse_mode=Markdown&text="+message
-    response = requests.get(send_text)
-    return response.json()
-    
